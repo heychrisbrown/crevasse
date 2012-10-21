84de53c543f8a89acfc5c2924e182db9afe0566a
Chris Brown <github@ktcb.com>
2012-10-21 17:38:10 -0500

    Use the arguments to specify from and to path

--- src/main/groovy/AmazonGlacierClient.groovy
+++ src/main/groovy/AmazonGlacierClient.groovy
@@ -1,41 +1,48 @@
 final projectName = 'crevasse'
-final vaultName = "family_media";
 final endpoint = "https://glacier.us-east-1.amazonaws.com/"
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-import com.amazonaws.auth.AWSCredentials;
-import com.amazonaws.auth.PropertiesCredentials;
+import com.amazonaws.auth.AWSCredentials
+import com.amazonaws.auth.PropertiesCredentials
 import com.amazonaws.services.glacier.transfer.ArchiveTransferManager
 import com.amazonaws.services.glacier.transfer.UploadResult
 import org.slf4j.*
 import org.apache.commons.cli.ParseException
 
 final dryrun = true
-ch.qos.logback.classic.Logger logger = (ch.qos.logback.classic.Logger) LoggerFactory.getLogger("AGC");
+ch.qos.logback.classic.Logger logger = (ch.qos.logback.classic.Logger) LoggerFactory.getLogger("AGC")
 
 def cli = new CliBuilder(
         usage: "${projectName} [options] --from [path] --to [vault]",
-        header: 'Options (or ask for --help):')
+        header: 'Options:')
 cli.with {
     h(longOpt: 'help', "this help")
     d(longOpt: 'debug', "enable debug logging")
-    from(longOpt: 'from', "the path to copy from")
-    to(longOpt: 'to', "the already-existing vault to copy to")
+    from(longOpt: 'from', args:1, argName:'path', 'the path to copy from')
+    to(longOpt: 'to', args:1, argName:'vault', "the already-existing vault to copy to")
 }
 def options = cli.parse(args)
 logger.setLevel(options.d ? ch.qos.logback.classic.Level.DEBUG : ch.qos.logback.classic.Level.INFO)
-if (options.h) cli.usage()
+if (options.h) {
+    cli.usage()
+    System.exit(0)
+}
+
+if (!(options.from && options.to)) {
+    cli.usage()
+    System.exit(1)
+}
+
 
 AWSCredentials credentials = new PropertiesCredentials(
         AmazonGlacierClient.class.getResourceAsStream("AwsCredentials.properties"))
 client = new com.amazonaws.services.glacier.AmazonGlacierClient(credentials)
 client.setEndpoint(endpoint)
 ArchiveTransferManager atm = new ArchiveTransferManager(client, credentials)
+logger.info("copying ${options.from} to ${credentials.AWSAccessKeyId}.${options.to}")
 
-dir = new File("/Users/cbrown/Pictures")
+dir = new File(options.from)
 if (!(dir.isDirectory())) {
-    println "${dir.canonicalPath} is not a directory."
+    logger.fatal "${dir.canonicalPath} is not a directory."
     System.exit(1)
 }
 
@@ -43,7 +50,7 @@ dir.traverse { archiveToUpload ->
     logger.debug("Processing ${archiveToUpload}")
     final description = "my archive " + (new Date())
     if (!dryrun) {
-        UploadResult result = atm.upload(vaultName, description, new File(archiveToUpload))
+        UploadResult result = atm.upload(options.to, description, new File(archiveToUpload))
         print "Archive ID: ${result.getArchiveId()}"
     }
 }
